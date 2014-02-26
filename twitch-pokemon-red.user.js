83e30c64e744a6efa5b155b3111450b6cd99480b
Paul Baker <son.of.lysander@gmail.com>
2014-02-26 01:51:13 -0700

    Test push

--- twitch-pokemon-red.user.js
+++ twitch-pokemon-red.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Twitch GM jQuery
 // @namespace  https://github.com/SonOfLysander
-// @version    0.452
+// @version    0.453
 // @description  Fight for anarchy!
 // @match      http://www.twitch.tv/twitchplayspokemon
 // @copyright  2012+, You
