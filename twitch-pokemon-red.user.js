2f15264a3bc43c890033c3abb31cb0b6b9513ac6
Paul Baker <son.of.lysander@gmail.com>
2014-02-26 01:04:44 -0700

    Fairly big overhaul. All chat specific methods are now 'encapsulated'.

--- twitch-pokemon-red.user.js
+++ twitch-pokemon-red.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Twitch GM jQuery
 // @namespace  https://github.com/SonOfLysander
-// @version    0.442
+// @version    0.443
 // @description  Fight for anarchy!
 // @match      http://www.twitch.tv/twitchplayspokemon
 // @copyright  2012+, You
@@ -10,13 +10,18 @@
 // ==/UserScript==
 
 var controller = {
-    messageInterval: null,
-    selfControllingInterval: null,
-    intervalMilliseconds: null,
+    _controllerInterval: null,
+    _randomizerInterval: null,
+    _timeLatencyMark: 0,
+    currentIntervalMill: null,
+    humanOptions: [    'Why am I still awake?', 'I wish I went to bed.', 'I need to go to bed.',
+                        'T_T', 'stop being so newb, gaiz.', 'Helix, save us from these spambots.'/*Not including me.*/,
+                        'Stop voting Democracy!', 'HELIXANDMOUNTAINDEWWILLSAVEMYGPA!!!',
+                        'Maybe you should go to the pokecenter for that BURN', 'Sleeeeeeeeeeeeeeeep'],
     createInterval: function() {
-        if(!this.messageInterval){
+        if(!this._controllerInterval){
             var newInterval = Math.floor(Math.random() * 12000) + 3000;
-            this.messageInterval = setInterval(function(){
+            this._controllerInterval = setInterval(function(){
                 if (isChatConnected()){
                     var msg = playerMessage();
                     $('#chat_speak').click(); //makes sure that you don't have anything in the "buffer" that will interfere with what we want to bot-in.
@@ -24,21 +29,54 @@ var controller = {
                     $('#chat_speak').click();
                 }
             }, newInterval);
-            this.intervalMilliseconds = newInterval;
+            this.currentIntervalMill = newInterval;
         }
-        if (!this.selfControllingInterval){
-            this.selfControllingInterval = setInterval(function(){controller.resetInterval()}, 3400);
+        if (!this._randomizerInterval){
+            this._randomizerInterval = setInterval(function(){controller.resetInterval()}, 3400);
         }
         console.log(this);
     },
     destroyInterval: function(){
-        if (this.messageInterval){
-            clearInterval(this.messageInterval);
+        if (this._controllerInterval){
+            clearInterval(this._controllerInterval);
         }
     },
     resetInterval:  function() {
         this.destroyInterval();
         this.createInterval();
+    },
+
+    _playerMessage: function(){
+        // I refactored because of the recent bot raid. I decided that even though I'm not doing
+        // anything particularly malicious, it would be best to put on a good face.
+        var msg = 'select';
+        var rnd = Math.random();
+        if (rnd < 0.97){ //97 percent of the time, we'll input a game command
+            rnd = Math.random(); //reroll for clear-er sub percentages
+            msg = rnd >= 0.50 ? 'anarchy' :
+            rnd >= 0.10 ? 'b' :
+            rnd >= 0.05 ? 'a' : 'select';
+        } else { //3 percent of the time
+            rnd = Math.floor(Math.random() * this.humanOptions.length);
+            msg = this.humanOptions[rnd];
+        }
+        return msg;
+    },
+    _isChatConnected: function(){
+        // This is an odd condictional structure, but it's really due to the
+        // fact Twitch has an odd flow that I can't access directly by API with
+        // out putting more effort into this script, and less effort into my home-
+        // work that matters.
+        if ($('#chat_text_input').prop('disabled') == true){
+            return false;
+        } else if ($('.js-chat_countdown').length == 0){
+            return true;
+        } else if (parseInt($('.js-chat_countdown:last').text(), 10) == 0 && Date.now() - this._timeLatencyMark >= 9000){
+            return true;               
+        } else {
+            this._timeLatencyMark = Date.now();
+            return false;
+        }
     }
 }
 
@@ -46,46 +84,6 @@ $(document).ready(function(){
     initializeDocument();
     controller.createInterval();
 });
-
-function playerMessage(){
-    // I refactored because of the recent bot raid. I decided that even though I'm not doing
-    // anything particularly malicious, it would be best to put on a good face.
-    var msg = 'select';
-    var rnd = Math.random();
-    if (rnd < 0.97){ //97 percent of the time, we'll input a game command
-        rnd = Math.random(); //reroll for clear-er sub percentages
-        msg = rnd >= 0.50 ? 'anarchy' :
-        rnd >= 0.10 ? 'b' :
-        rnd >= 0.05 ? 'a' : 'select';
-    } else { //3 percent of the time
-        var options = [ 'Why am I still awake?', 'I wish I went to bed.', 'I need to go to bed.',
-                        'T_T', 'stop being so newb, gaiz.', 'Helix, save us from these spambots.'/*Not including me.*/,
-                        'Stop voting Democracy!', 'HELIXANDMOUNTAINDEWWILLSAVEMYGPA!!!',
-                        'Maybe you should go to the pokecenter for that BURN', 'Sleeeeeeeeeeeeeeeep'];
-        rnd = Math.floor(Math.random() * options.length);
-        msg = options[rnd];
-    }
-    return msg;
-}
-
-var timeLatencyMark = 0;
-
-function isChatConnected(){
-    // This is an odd condictional structure, but it's really due to the
-    // fact Twitch has an odd flow that I can't access directly by API with
-    // out putting more effort into this script, and less effort into my home-
-    // work that matters.
-    if ($('#chat_text_input').prop('disabled') == true){
-        return false;
-    } else if ($('.js-chat_countdown').length == 0){
-        return true;
-    } else if (parseInt($('.js-chat_countdown:last').text(), 10) == 0 && Date.now() - timeLatencyMark >= 9000){
-        return true;               
-    } else {
-        timeLatencyMark = Date.now();
-        return false;
-    }
-}
 
 function initializeDocument(){ //Adds CSS styles for myself and one of my friends so I can see the chats as they wiz by.
     hideall();
